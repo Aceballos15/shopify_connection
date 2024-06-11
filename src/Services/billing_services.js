4831aecf5c38b2cae3bb5930ba4997b71a4880c4
Aceballos <ceballoscardonaalexander@gmail.com>
2024-06-11 13:42:35 -0500

    Add new

--- src/Services/billing_services.js
+++ src/Services/billing_services.js
@@ -26,6 +26,7 @@ class billingService {
       const billing_detail = zoho_order.data.data[0];
       const tracking_detail = despacho.data.data[0];
 
+      console.log(billing_detail.data.data)
       const detailOrder = await axios.get(
         `${BASE_URI_ZOHO}/shopifyDetail?where=ordenID==${billing_detail.ID}`
       );
